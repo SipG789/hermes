37bdfecff677ca05497fb952e5f70060c40c6181
Sip <sipg789@gmail.com>
2022-04-21 15:44:07 -0500

    pseudo code added

---
+++ server.js
@@ -0,0 +1,18 @@
+// TODO: getting started
+
+// TODO: require fs, express, apiroutes, ports, json 
+
+// TODO: GET /notes route should return notes.html
+
+// TODO: GET * route should return index.html
+
+// TODO: Create API's
+
+// TODO: GET /api/notes should read the db.json file and return all saved notes as JSON
+
+// TODO: POST /api/notes should receive a new note to save on the request body, add it to the db.json file & return the new note to the client 
+
+// TODO: Find a way to give each note a unique id when it's saved (look into npm packages that could do this)
+
+// TODO: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
+    //TODO: read all notes from the db.json file, remove the note with the given id property, then rewrite the notes to the db.json file. 
