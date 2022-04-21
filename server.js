d8a2a463557d6d54dca9bd447ccfa5f0698f31c2
Sip <sipg789@gmail.com>
2022-04-21 16:16:35 -0500

    created express server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // TODO: getting started
 
 // TODO: require fs, express, apiroutes, ports, json 
+const express = require('express');
+const { notes } = require('./Develop/db/db.json');
 
-// TODO: GET /notes route should return notes.html
+// instantiate server
+const app = express();
+
+
 
 // TODO: GET * route should return index.html
 
@@ -16,3 +21,16 @@
 
 // TODO: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
     //TODO: read all notes from the db.json file, remove the note with the given id property, then rewrite the notes to the db.json file. 
+
+// TODO: GET /notes route should return notes.html
+app.get('/api/notes', (req, res) => {
+    res.send('Hello');
+});
+
+
+
+
+// express listen 
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
