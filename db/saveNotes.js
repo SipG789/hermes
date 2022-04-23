1dbc72500a6ea96163a666c0722890cbd075c9fa
Sip <sipg789@gmail.com>
2022-04-23 14:25:28 -0500

    added routes

---
+++ db/saveNotes.js
@@ -0,0 +1,7 @@
+// TODO: create dependencies
+
+// TODO: save notes into db.json (use stringify???)
+
+// TODO: retrieve notes
+
+// TODO: [bonus] delete notes 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // TODO: require fs, express, apiroutes, ports, json 
 
 const express = require('express');
-const { notes } = require('./db/db.json');
+const notes = require('./db/db.json');
 const path = require('path');
 
 // instantiate server
@@ -15,19 +15,23 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+
 // TODO: GET / notes route should return notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-// TODO: Create API's
 
-// TODO: GET /api/notes should read the db.json file and return all saved notes as JSON
+
+// Create API's
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
 // TODO: POST /api/notes should receive a new note to save on the request body, add it to the db.json file & return the new note to the client 
-
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    req.body.id = notes.length.toString();
+});
 // TODO: Find a way to give each note a unique id when it's saved (look into npm packages that could do this)
 
 // TODO: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
@@ -36,7 +40,9 @@ app.get('*', (req, res) => {
 
 
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 // express listen 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
